99434b0ea7aa05a8dcbea254757859f3d4a59641
Jevona Magdalena <jmagdalena@ictual.com>
2023-03-20 14:20:56 +0100

    styling in

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 // Setup the template engine ejs
 // app.set('views', path.join(_dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+app.use(express.static('assets'))
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.use('/static', express.static(path.join(__dirname, 'assets')))
+
 
 app.get('/', function(req, res) {
     res.render('pages/index')
