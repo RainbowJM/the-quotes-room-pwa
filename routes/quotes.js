3c3fc66f7846e6c2d2626c1edba1e4a4ae8994eb
Jevona Magdalena <jmagdalena@ictual.com>
2023-03-21 23:08:05 +0100

    working on read me and  detail page

--- routes/quotes.js
+++ routes/quotes.js
@@ -3,34 +3,35 @@ const request = require('request');
 
 const router = express.Router();
 
+// Overview page
 router.get('/', function(req, res) {
 	request('https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes', {json: true}, function (err, requestRes, body){
 		if (err) {
 			res.send(err);
 		} else {
-            console.log("hello")
-			res.render('index', {
-				title: 'Home', // We use this for the page title, see views/partials/head.ejs
+			res.render('quotes', {
+				title: 'Home', 
 				data: body
 			});
+            // console.log(body);
 		}
 	});
 });
 
-// Create a route for our detail page
-// router.get('/quotes/:id', function(req, res) {
-// 	request(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`, {json: true}, function (err, requestRes, body){
-// 		if (err) {
-// 			// We got an error
-// 			res.send(err);
-// 		} else {
-// 			// Render the page using the 'post' view and our body data
-// 			res.render('post', {
-// 				title: `Post ${req.params.id}`, 
-// 				postData: body
-// 			});
-// 		}
-// 	});
-// });
+// Detail page
+router.get('/quotes/:id', function(req, res) {
+	request(`https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes/${req.params.id}`, {json: true}, function (err, requestRes, body){
+		if (err) {
+			res.send(err);
+		} else {
+			// Render the page using the 'post' view and our body data
+			res.render('quote', {
+				title: `Quote ${req.params.id}`, 
+				data: body
+			});
+            console.log(body)
+		}
+	});
+});
 
 module.exports = router;
