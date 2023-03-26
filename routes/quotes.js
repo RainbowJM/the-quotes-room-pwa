11bd13763a4bc371b9af6544a1e9f26908494870
Jevona Magdalena <jmagdalena@ictual.com>
2023-03-26 15:56:14 +0200

    detail page done

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const request = require('request');
-
+const axios = require('axios');
+const url = 'https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes';
 const router = express.Router();
 
 // Overview page
 router.get('/', function(req, res) {
-	request('https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes', {json: true}, function (err, requestRes, body){
+	request(url, {json: true}, function (err, requestRes, body){
 		if (err) {
 			res.send(err);
 		} else {
@@ -13,25 +14,27 @@ router.get('/', function(req, res) {
 				title: 'Home', 
 				data: body
 			});
-            // console.log(body);
 		}
 	});
 });
 
 // Detail page
 router.get('/quotes/:id', function(req, res) {
-	request(`https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes/${req.params.id}`, {json: true}, function (err, requestRes, body){
-		if (err) {
-			res.send(err);
-		} else {
-			// Render the page using the 'post' view and our body data
-			res.render('quote', {
-				title: `Quote ${req.params.id}`, 
-				data: body
-			});
-            console.log(body)
-		}
-	});
+    const id = req.params.id
+    let quotes;
+    let quote;
+    axios.get(url)
+    .then(function(response) {
+        quotes = response.data;
+        quote = quotes.find(quote => quote.id === id);
+
+        console.log(quote)
+        if (quote !== null){
+            res.render('quote', {
+                data: quote
+            });
+        }
+    });
 });
 
 module.exports = router;
