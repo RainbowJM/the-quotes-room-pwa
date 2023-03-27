c556bfcbdd7afa8f57172498d661ee94a9004b43
Jevona Magdalena <jmagdalena@ictual.com>
2023-03-27 22:29:34 +0200

    added offline and working on caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,24 @@
+const cacheName = 'site-static';
+const assets = [
+    '/',
+    '/css/style.css',
+    // '/offline.ejs'
+
+]
 
 self.addEventListener('install', event => {
-  console.log('Installing service worker')
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log('Installed service worker')
+            return cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
-  console.log('Activating service worker')
+    // console.log('Activating service worker')
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event: ', event.request.url);
+    // console.log('Fetch event: ', event);
 });
